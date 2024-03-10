8fc298060456a6cd55c3f567bc8a6d093c969093
Duvan Mancilla <Duvanred0@gmail.com>
2024-03-10 21:35:46 +0100

    Footer nuevo diseño

--- src/app/Componentes/Footer.tsx
+++ src/app/Componentes/Footer.tsx
@@ -1,48 +1,45 @@
 'use client'
-import Image from "next/image"
-import Instagram from '../../../public/multimedia/instagram.png'
+import Linkedin from '../Icon/Linkedin'
+import Instagram from '../Icon/Instagram'
+import { useEffect, useState} from "react"
+import Link from 'next/link'
 const Footer = () => {
+
+    //Para evitar problemas de Hydration se espera a que cargue el componente antes de ser mostrado
+    const [isLoaded, setIsloaded] = useState(false)
+
+    useEffect(() => {
+        setIsloaded(true)
+    },[])
+
+    if(!isLoaded) return null
+ 
     return(
-       <>
-       <footer className="bg-fondoNegro flex flex-col justify-center items-center text-center text-fondoBlanco gap-3 p-10 overflow-hidden">
-        <h4 className="text-lg">¡Trabajemos Juntos!</h4>
-        <picture className="flex flex flex-row justify-center items-center gap-4">
-        <hr  className="min-w-[500px]"></hr>
-        <a href="">
-        <Image
-        src={Instagram}
-        width={32}
-        height={32}
-        alt="Instagram"
-        >
-            
-        </Image>
-        </a>
-        <a href="">
-        <Image
-        src={Instagram}
-        width={32}
-        height={32}
-        alt="Instagram"
-        >
-            
-        </Image>
-        </a>
-        <a href="">
-        <Image
-        src={Instagram}
-        width={32}
-        height={32}
-        alt="Instagram"
-        >
-            
-        </Image>   
-        </a>
-        <hr className="min-w-[500px]"></hr>
-        </picture>
-        <p>Prohibida la reproducción total o parcial de este sitio y su contenido.</p>
-        <p className="font-bold">SantiCuorePH Copyright &copy; 2024. Buenos Aires, Argentina.</p>
-       </footer>
+    <>
+    <footer className="bg-fondoNegro p-8 pb-10 text-principal text-sm">
+    <div className="flex flex-col items-start justify-center gap-10 md:gap-0 md:flex-row md:justify-between md:items-center">
+    <div className="flex flex-col gap-2">
+    <picture className="flex gap-4">   {/*Semantica Picture en los iconos para evitar uso excesivo de DIVs*/}
+    <Instagram />
+    <Linkedin /> 
+    {/*SVG Instagram y Linkedin*/}
+    </picture>
+    <address className='not-italic'> {/*semantica address para contenedor correo eletronico, la etiqueta pone por defecto el texto en
+    italic, por lo cual, especifico not-italic*/}
+        <a href="mailto:Santiph@gmail.com">Mail: Santiph@gmail.com</a>   {/*Mailto para clickear y abrir app correos*/}
+    </address>
+    </div>
+    <nav>   {/*Nav en footer ya que es un apartado de enlaces de navegacion*/}
+        <ul className="flex flex-col gap-1.5">
+            <li><Link href={""}>Acerca de</Link></li>
+            <li><Link href={""}>Contacto</Link></li>
+            <li><Link href={""}>Aviso Legal</Link></li>
+        </ul>
+    </nav>
+    </div>
+    <p className="text-center pt-6">Santi cuore Copyright 2024</p>
+</footer>
+
        </> 
     )
 }
