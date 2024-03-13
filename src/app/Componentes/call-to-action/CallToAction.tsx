9ff288c39253b10f7a75ca33dc7dd6002dba3936
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-03-13 18:51:44 -0300

    Change font size contactme section

--- src/app/Componentes/call-to-action/CallToAction.tsx
+++ src/app/Componentes/call-to-action/CallToAction.tsx
@@ -15,7 +15,7 @@ const CallToAction = (props: CallToActionProps) => {
     const {header, imageUrl, buttonText, imageSize} = props;
 
     return (
-        <section className="mx-auto text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl flex flex-col justify-center items-center gap-[1.25em] md:gap-[1.25em] relative">
+        <section className="mx-auto text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl flex flex-col justify-center items-center gap-[1.25em] md:gap-[1.25em] relative">
             <p className="text-center font-bold merriwather">{header}</p>
             <div className="relative w-3/12 min-w-[150px] max-w-[410px] aspect-square" >
             <Image
@@ -28,7 +28,7 @@ const CallToAction = (props: CallToActionProps) => {
                 title="Contactame"
                 styles="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl py-[0.5em] px-[2em] bg-principal rounded-full merriwather font-bold text-fondoNegro hover:bg-[#FBBF01] hover:scale-110" />
                 */}
-                <Link href="/contacto" className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl py-[0.5em] px-[2em] bg-principal rounded-full merriwather font-bold text-fondoNegro hover:bg-[#FBBF01] hover:scale-110">
+                <Link href="/contacto" className="text-xl sm:text-2xl md:text-3xl lg:text-4xl py-[0.5em] px-[2em] bg-principal rounded-full merriwather font-bold text-fondoNegro hover:bg-principalHover hover:scale-110">
                     Contactame
                 </Link>
                 
