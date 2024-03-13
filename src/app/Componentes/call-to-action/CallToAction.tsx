1d7b28490c2802a67a39fcb8b68b9e6673b83767
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-03-12 21:40:16 -0300

    Done call to action home

--- src/app/Componentes/call-to-action/CallToAction.tsx
+++ src/app/Componentes/call-to-action/CallToAction.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import CustomButton from "../CustomButton";
 
 type CallToActionProps = {
     header: string,
@@ -12,16 +13,18 @@ const CallToAction = (props: CallToActionProps) => {
     const {header, imageUrl, buttonText, imageSize} = props;
 
     return (
-        <section className="flex flex-col justify-center items-center gap-8">
-            <p className="text-3xl font-bold">{header}</p>
-            <div className="relative container mw-xl" >
+        <section className="flex flex-col justify-center items-center gap-[85px] relative">
+            <p className="text-6xl font-bold merriwather">{header}</p>
+            <div className="relative w-3/12 max-w-[410px] min-w-[200px] aspect-square" >
             <Image
             src={imageUrl}
             fill={true}
             alt="Picture of Santi Cuore"
             />
             </div>
-            <button>{buttonText}</button>
+            <CustomButton
+                title="Contactame"
+                styles="py-4 bg-principal rounded-full merriwather font-bold text-fondoNegro text-4xl w-3/12 max-w-[410px] min-w-[200px]" />
         </section>
     )
 }
