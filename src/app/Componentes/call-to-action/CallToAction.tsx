f24d92af8d6acb7a5c302457f4f6741fefba76b2
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-03-13 13:41:21 -0300

    Implemented responsive layout for 'Contactame' section on homepage

--- src/app/Componentes/call-to-action/CallToAction.tsx
+++ src/app/Componentes/call-to-action/CallToAction.tsx
@@ -7,15 +7,16 @@ type CallToActionProps = {
     buttonText: string,
     imageSize: number
 }
+//w-1/5 max-w-[410px] min-w-[200px]
 
 const CallToAction = (props: CallToActionProps) => {
 
     const {header, imageUrl, buttonText, imageSize} = props;
 
     return (
-        <section className="flex flex-col justify-center items-center gap-[85px] relative">
-            <p className="text-6xl font-bold merriwather">{header}</p>
-            <div className="relative w-3/12 max-w-[410px] min-w-[200px] aspect-square" >
+        <section className="mx-auto text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl flex flex-col justify-center items-center gap-[1.25em] md:gap-[1.25em] relative">
+            <p className="text-center font-bold merriwather">{header}</p>
+            <div className="relative w-3/12 min-w-[150px] max-w-[410px] aspect-square" >
             <Image
             src={imageUrl}
             fill={true}
@@ -24,7 +25,7 @@ const CallToAction = (props: CallToActionProps) => {
             </div>
             <CustomButton
                 title="Contactame"
-                styles="py-4 bg-principal rounded-full merriwather font-bold text-fondoNegro text-4xl w-3/12 max-w-[410px] min-w-[200px]" />
+                styles="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl py-[0.5em] px-[2em] bg-principal rounded-full merriwather font-bold text-fondoNegro" />
         </section>
     )
 }
