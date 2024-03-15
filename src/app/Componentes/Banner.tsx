5830d0b32961ed19ec10bb47798cca3c08435f39
MartinezNicolass <martinezz.nnicolas@gmail.com>
2024-03-14 23:06:56 -0300

    video changes, responsive design

--- src/app/Componentes/Banner.tsx
+++ src/app/Componentes/Banner.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState } from "react";
 const Banner = () => {
   const heigthCard = 500;
   const [videoImage, setVideoImage] = useState(true);
+
+
   const video0Ref = useRef<HTMLVideoElement>(null);
   const [video0Playing, setVideoPlaying] = useState(false);
   const handleVideo = () => {
@@ -14,11 +16,13 @@ const Banner = () => {
     }
     setVideoPlaying(!video0Playing);
   };
+
+
   const video1Ref = useRef<HTMLVideoElement>(null);
-  useEffect(()=>{
-video1Ref.current?.pause()
-video0Ref.current?.pause()
-  },[])
+  useEffect(() => {
+    video1Ref.current?.pause();
+    video0Ref.current?.pause();
+  }, []);
   const [video1Playing, setVideo1Playing] = useState(false);
   const handleVideo1 = () => {
     if (video1Playing) {
@@ -30,11 +34,11 @@ video0Ref.current?.pause()
   };
 
   return (
-    <section className="h-max px-2 space-y-2 bg-black">
+    <section className="h-screen font-merriwather text-white  bg-black">
       <div
         onMouseEnter={handleVideo}
         onMouseLeave={handleVideo}
-        className={`relative hover:scale-[1.012] col-span-1 my-auto flex   w-full h-[${heigthCard}px]  bg-white transition-all ease-in-out hover:shadow-2xl smm:right-0 smm:top-15  smm:col-span-4   smm:h-[250px]`}
+        className={`relative hover:scale-[0.97]  flex   w-full h-1/2   transition-all ease-in-out    `}
       >
         {!videoImage ? (
           <img
@@ -44,28 +48,25 @@ video0Ref.current?.pause()
           />
         ) : (
           <video
-          className="w-full object-cover"
-          loop
-          autoPlay
+            className="w-full object-cover"
+            loop
+            autoPlay
             ref={video0Ref}
             muted
-            
-            src="https://vod-progressive.akamaized.net/exp=1710300796~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F508%2F18%2F452544778%2F1991951834.mp4~hmac=5deebafb47f76f68c01dad58f084824fd69b7137ff33aab95761e74c274e0930/vimeo-prod-skyfire-std-us/01/508/18/452544778/1991951834.mp4?filename=file.mp4"
+            src="https://vod-progressive.akamaized.net/exp=1710479431~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3651%2F14%2F368256550%2F1523668912.mp4~hmac=82b6437dc2f72299dd25092d4522a7cf16bc8c49fa4c93eb13e1570a02d3aeb9/vimeo-prod-skyfire-std-us/01/3651/14/368256550/1523668912.mp4?filename=file.mp4"
           />
         )}
 
-        <div className="absolute top-0 z-30 flex h-full w-full transition-all duration-200 hover:bg-black/50 ">
-          <div className="flex flex-col  p-5 ">
-            <h1 className="pb-2  text-5xl font-bold text-white smm:text-xl ">
-              Fotografías
-            </h1>
-          </div>
+        <div className="absolute p-5 right-0 left-0 top-0 z-30 flex h-full w-full transition-all duration-200 hover:bg-black/50 ">
+          <h1 className="text-[2rem] md:text-[3rem] lg:text-[4rem] italic  ">
+            Fotografías
+          </h1>
         </div>
       </div>
       <div
-       onMouseEnter={handleVideo1}
-       onMouseLeave={handleVideo1}
-        className={`relative hover:scale-[1.012] col-span-1 my-auto flex   w-full h-[${heigthCard}px]  bg-white transition-all ease-in-out hover:shadow-2xl smm:right-0 smm:top-15  smm:col-span-4   smm:h-[250px]`}
+        onMouseEnter={handleVideo1}
+        onMouseLeave={handleVideo1}
+        className={`relative hover:scale-[0.97]  flex   w-full h-1/2   transition-all ease-in-out `}
       >
         {!videoImage ? (
           <img
@@ -75,20 +76,18 @@ video0Ref.current?.pause()
           />
         ) : (
           <video
-            className="w-full object-cover"
+            className="w-full object-cover object-bottom"
             loop
             autoPlay
             ref={video1Ref}
             muted
-            src="https://vod-progressive.akamaized.net/exp=1710299839~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2331%2F21%2F536658081%2F2539702061.mp4~hmac=3b2e75f30f5f44a8d21c7f207183927a49aff53b49dc08394e9751e6ff8385a7/vimeo-prod-skyfire-std-us/01/2331/21/536658081/2539702061.mp4?filename=file.mp4"
+            src="https://vod-progressive.akamaized.net/exp=1710479932~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F711%2F14%2F353558999%2F1436805237.mp4~hmac=93852619386b969a935f35e0e99b6fbdd84614171e08de698693761be654967e/vimeo-prod-skyfire-std-us/01/711/14/353558999/1436805237.mp4?filename=file.mp4"
           />
         )}
-        <div className="absolute top-0 z-30 flex h-full w-full transition-all duration-200 hover:bg-black/50 ">
-          <div className="flex flex-col  p-5 ">
-            <h1 className="pb-2  text-5xl font-bold text-white smm:text-xl ">
-              Edición Audiovisual
-            </h1>
-          </div>
+        <div className="absolute p-5 top-0 z-30 flex h-full w-full transition-all duration-200 hover:bg-black/50 ">
+          <h1 className="text-[2rem] md:text-[3rem] lg:text-[4rem] italic   ">
+            Edición Audiovisual
+          </h1>
         </div>
       </div>
     </section>
