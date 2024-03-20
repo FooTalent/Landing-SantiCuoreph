d52bf1337f81a8c001baa9b481a4065a1adaf6c4
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-03-19 21:09:12 -0300

    Add animation to open-close menu

--- src/app/Componentes/header/Header.tsx
+++ src/app/Componentes/header/Header.tsx
@@ -6,6 +6,12 @@ import TextLogo from "./TextLogo";
 import { useState } from "react";
 import Logo from "./Logo";
 import Whatsapp from "@/app/Icon/Whatsappequipo6";
+import FacebookLogo from "./FacebookLogo";
+import LogoSanti from "../../../../public/images/navbar/logo-santi.png";
+import Image from "next/image";
+import MenuOpen from "./MenuOpen";
+import MenuClose from "./MenuClose";
+import "./styles.css";
 
 const navLinks = [
   { name: "Servicios", href: "/servicios" },
@@ -23,43 +29,55 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-fondoNegro fixed z-40 w-full">
-      <nav className="py-4 pr-4 lg:px-40">
+    <header className="bg-fondoNegro fixed z-40 w-full opacity-85">
+      <nav className="py-3 pr-4 lg:px-40 opacity-100">
         <div className="flex justify-between">
-          <div className="flex items-center grow">
+          <div className="flex items-center grow-[2]">
             <div className="flex-shrink-0">
               <Link href="/" className="flex items-center gap-2 grow">
-                <Logo></Logo>
+                <Image src={LogoSanti} alt="Logo Santi Cuore PH" />
               </Link>
             </div>
           </div>
 
-          <ul className="hidden sm:flex justify-between items-center text-white grow text-xl">
+          <ul className="hidden sm:flex justify-around items-center text-white text-xl grow-[2] pr-40">
             {navLinks.map((link) => {
               return (
-                <li className="hover:text-principalHover font-semibold hover:font-bold font-nunitoSans" key={link.name}>
+                <li
+                  className="hover:text-principalHover font-semibold hover:font-bold font-nunitoSans"
+                  key={link.name}
+                >
                   <Link href={link.href}>{link.name}</Link>
                 </li>
               );
             })}
           </ul>
-          <div className="hidden sm:flex grow justify-end items-center gap-10">
-            <a href="https://www.instagram.com/santicuoreph" target="_blank">
-              <InstagramLogo size={32} />
-            </a>
-          </div>  
-          <div className="hidden sm:flex grow justify-end items-center gap-10">
-            <a href="" target="_blank">
-              <Whatsapp />
-            </a>
+          <div className="flex gap-10">
+            <div className="hidden sm:flex grow justify-end items-center gap-10">
+              <a href="https://www.instagram.com/santicuoreph" target="_blank">
+                <InstagramLogo size={32} />
+              </a>
+            </div>
+            <div className="hidden sm:flex grow justify-end items-center gap-10">
+              <a href="" target="_blank">
+                <Whatsapp />
+              </a>
+            </div>
+            <div className="hidden sm:flex grow justify-end items-center gap-10">
+              <a href="" target="_blank">
+                <FacebookLogo />
+              </a>
+            </div>
           </div>
-          <div className="sm:hidden flex items-center">
-            <button
-              className="inline-flex items-center justify-center"
-              onClick={toggleNavbar}
-            >
-             {isClick ? "ABRIR" : "CERRAR"}
-            </button>
+          <div className="sm:hidden flex items-center relative" onClick={toggleNavbar}>
+              <div className="box">
+                <div className={`btn ${isClick ? 'active' : 'not-active'}`}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+            
           </div>
         </div>
 
@@ -83,11 +101,8 @@ const Header = () => {
                   </a>
                 </div>
                 <div className="grow justify-end grow">
-                  <a
-                    href=""
-                    target="_blank"
-                  >
-                      <Whatsapp />
+                  <a href="" target="_blank">
+                    <Whatsapp />
                   </a>
                 </div>
               </ul>
