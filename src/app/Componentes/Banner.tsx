9340495206d350492c3280237046fb945421501b
Duvan Mancilla <Duvanred0@gmail.com>
2024-03-20 18:06:56 -0500

    banner con zoom correcto y demas requisitos

--- src/app/Componentes/Banner.tsx
+++ src/app/Componentes/Banner.tsx
@@ -5,43 +5,47 @@ import bannerFotografia from '../../../public/images/banner.jpg'
 const Banner = () => {
 
   return (
-    <section className="h-max px-2 space-y-2 font-merriwather text-white  bg-black overflow-hidden">
+    <>
+    <section className="h-max px-2 space-y-2 font-merriwather text-white  bg-black overflow-hidden relative">
+      <div className="absolute p-5 pt-16 right-0 left-0 top-0 z-30 flex h-fit w-fit">
+          <h2 className="text-[2rem] md:text-[3rem] lg:text-[4rem] lg:ml-20 ml-4  merriwather">
+            Fotografía
+          </h2>
+        </div>
       <div
-        className={`relative hover:scale-[1.2]  flex   w-full h-[500px]   transition-all ease-in-out    `}
+        className={`relative hover:scale-[1.2] flex w-full h-[500px]  transition-all ease-in-out cursor-pointer   `}
       >
        <Image
        src={bannerFotografia}
-       width={1536}
+       width={4036}
        height={500}
        alt="terraza"
        >
 
        </Image>
 
-        <div className="absolute p-5 pt-16 right-0 left-0 top-0 z-30 flex h-full w-full transition-all duration-300 hover:bg-black/50 ">
-          <h2 className="text-[2rem] md:text-[3rem] lg:text-[4rem] lg:ml-20 ml-4 ">
-            Fotografía
+      </div>
+    </section>
+    <section className="h-max px-2 space-y-2 font-merriwather text-white  bg-black overflow-hidden relative">
+      <div className="absolute p-5 pt-16 top-0 z-30 flex h-fit w-fit">
+          <h2 className="text-[2rem] md:text-[3rem] lg:text-[4rem] lg:ml-20 ml-4 merriwather ">
+            Edición Audiovisual
           </h2>
         </div>
-      </div>
       <div                
-        className={`relative hover:scale-[1.2]  flex   w-full h-[500px]   transition-all ease-in-out `}
+        className={`relative hover:scale-[1.2]  flex  w-full h-[500px]   transition-all ease-in-out cursor-pointer `}
       >
           <video
             className="w-full object-cover object-bottom"
             loop
             autoPlay
             muted
-            src="https://player.vimeo.com/progressive_redirect/playback/348728035/rendition/1080p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=9fd0f1b42276d7d0cdee48f924811ccb3f8288807f1504c5866b4b800fd8e02d"
+            src="https://s3-figma-videos-production-sig.figma.com/video/1159262127674411291/TEAM/17de/6cd1/-61a8-4781-bdef-fd462c983666?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FHXQSgeVM5W9QF9udObiHm53zb6oZ1egVSEpdMoibsRdgf-a8tcw~mNYEiP~Mf7cgQswr5zyaeU1TA7wp-VsLTZPCb2f1iMEk85HIr2SwiEUnjti-w9TR9jNfi8HSnuRWpmxXWqHVjxxhwI5l14faOux0CRYcoFlncNiUmdoT8WZGXAnjod2pCqJtXbOoUDOQdoKJaPgdO6aCGzp6RhN1z2vEmzxKOFNyCiSFW~2QsL-IPjh~vpY14Hk0vW8SEzV1Q3XkIv3K-u0MU8staPync735z8LPCvj8puy5uDs2-M8f6PfMjTR9rVlmr-Et~NlN8ReGhivz~tQ~0aI5maQSQ__"
             
           />
-        <div className="absolute p-5 pt-16 top-0 z-30 flex h-full w-full  transition-all duration-200 hover:bg-black/50 ">
-          <h2 className="text-[2rem] md:text-[3rem] lg:text-[4rem] lg:ml-20 ml-4  ">
-            Edición Audiovisual
-          </h2>
-        </div>
       </div>
     </section>
+    </>
   );
 };
 
