9a5172944c53c4e2bdaad27c2e08dc80b42021a0
Duvan Mancilla <Duvanred0@gmail.com>
2024-03-20 18:16:29 -0500

    cambio hover en Nav a color secundario no principal hover y borrado de facebook en nav

--- src/app/Componentes/header/Header.tsx
+++ src/app/Componentes/header/Header.tsx
@@ -44,7 +44,7 @@ const Header = () => {
             {navLinks.map((link) => {
               return (
                 <li
-                  className="hover:text-principalHover font-semibold hover:font-bold font-nunitoSans"
+                  className="hover:text-secundario font-semibold hover:font-bold font-nunitoSans"
                   key={link.name}
                 >
                   <Link href={link.href}>{link.name}</Link>
@@ -61,11 +61,6 @@ const Header = () => {
             <div className="hidden sm:flex grow justify-end items-center gap-10">
               <a href="" target="_blank">
                 <Whatsapp />
-              </a>
-            </div>
-            <div className="hidden sm:flex grow justify-end items-center gap-10">
-              <a href="" target="_blank">
-                <FacebookLogo />
               </a>
             </div>
           </div>
@@ -101,9 +96,6 @@ const Header = () => {
                   </a>
                   <a href="" target="_blank">
                     <Whatsapp />
-                  </a>
-                  <a href="" target="_blank">
-                    <FacebookLogo />
                   </a>
                 </div>
                 <div className="grow justify-end grow">
