01c8cd70461053afb2eadd55ab499c8c488056bd
rafaric <rafaelstrongoli@gmail.com>
2024-04-02 17:22:16 -0300

    responsive. smooth scrool on page change cambios en navbar.

--- src/app/contacto/formulario/page.tsx
+++ src/app/contacto/formulario/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+import { useState } from "react";
+import CustomButton from "@/app/Componentes/CustomButtonequipo6";
 import Form from "../../Componentes/Form/form";
+import { useRouter } from "next/navigation";
 const Contact = () => {
+  const navigate = useRouter();
+  const [paso, setPaso] = useState<number>(0);
   return (
     <div className="">
-      <div id="formulario" className="py-40 px-56">
-        <Form />
+      <div
+        id="formulario"
+        className="pt-28 pb-28 md:py-40 px-5 md:px-16 lg:px-32"
+      >
+        <div className="flex">
+          <CustomButton
+            title=""
+            styles="w-[30px] h-30px] mr-4"
+            onClick={() => {
+              if (paso === 0) {
+                navigate.push("/contacto");
+              } else {
+                setPaso(paso - 1);
+              }
+            }}
+          />
+          <h1 className="font-merriwather text-2xl sm:text-4xl lg:text-6xl font-bold text-fondoBlanco">
+            Solicitud de presupuesto
+          </h1>
+        </div>
+        <div className="border-[1.5px] border-fondoBlanco my-9 "></div>
+        <Form paso={paso} setPaso={setPaso} />
       </div>
     </div>
   );
