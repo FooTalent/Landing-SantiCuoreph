f4b3bf6af802c2677316974f2b6f8345432ea388
rafaric <rafaelstrongoli@gmail.com>
2024-04-05 16:53:22 -0300

    cambio de orden de los textos en slider y linkeo de los mismos a sus servicios

--- src/app/Componentes/Slideshow/Slider.tsx
+++ src/app/Componentes/Slideshow/Slider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
@@ -8,25 +9,33 @@ import "react-slideshow-image/dist/styles.css";
 
 const slideImages = [
   {
+    index: 1,
     url: "/assets/img/Masq1.JPG",
     caption: "Más que uno: 10 años",
     caption2: "Cobertura de evento artístico",
+    link: "/fotografia/mas-que-uno",
   },
   {
+    index: 2,
     url: "/assets/img/AuraGin.JPG",
     caption: "Aura Gin",
     caption2: "Fotografía comercial",
+    link: "/fotografia/aura-gin",
   },
   {
+    index: 3,
     url: "/assets/img/PasadoPisado.JPG",
     caption: "Pasado Pisado",
     caption2: "Fotografía artística",
+    link: "/fotografia/pasado-pisado",
   },
 
   {
+    index: 4,
     url: "/assets/img/Toch.JPG",
     caption: "TOCH",
     caption2: "Cobertura de evento artístico",
+    link: "/servicios/audiovisual",
   },
 ];
 
@@ -40,13 +49,7 @@ const properties = {
   prevArrow: (
     <button className="lg:ml-[115px] ml-4 " style={{ ...buttonStyle }}>
       <div className="bg-slider w-14 h-14 flex justify-center items-center hover:bg-sliderHover rounded-full">
-        <Image
-          src="/assets/img/ArrowLeft.png"
-          alt=""
-          style={{ width: "auto", height: "auto" }}
-          width={20}
-          height={20}
-        />
+        <Image src="/assets/img/ArrowLeft.png" alt="" width={20} height={20} />
       </div>
     </button>
   ),
@@ -71,27 +74,29 @@ const Slideshow = () => {
         cssClass="pb-5"
       >
         {slideImages.map((slideImage, index) => (
-          <div key={index} className="h-full">
-            <div className="h-screen relative flex items-center">
-              <Image
-                src={slideImage.url}
-                alt="Slide"
-                width="0"
-                height="0"
-                sizes="100%"
-                className={`w-screen h-full object-cover ${
-                  index == 0 || index == 3 ? "object-top" : ""
-                } filter brightness-50`}
-              />
+          <div key={slideImage.index} className="h-full">
+            <Link href={slideImage.link}>
+              <div className="h-screen relative flex items-center">
+                <Image
+                  src={slideImage.url}
+                  alt="Slide"
+                  width="0"
+                  height="0"
+                  sizes="100%"
+                  className={`w-screen h-full object-cover ${
+                    index == 0 || index == 3 ? "object-top" : ""
+                  } filter brightness-50`}
+                />
 
-              {/* TEXTO PRINCIPAL Y SECUNDARIO */}
-              <p className="text-fondoBlanco absolute font-merriwather font-bold italic lg:text-[4rem] text-[2rem] lg:left-[105px] left-4 lg:bottom-32 bottom-48">
-                {slideImage.caption}
-              </p>
-              <p className="lg:left-[105px] font-nunitoSans absolute font-light text-fondoBlanco md:text-[2rem] text-[1.5rem] left-4 lg:bottom-24 bottom-36">
-                {slideImage.caption2}
-              </p>
-            </div>
+                {/* TEXTO PRINCIPAL Y SECUNDARIO */}
+                <p className="text-fondoBlanco absolute font-merriwather font-bold italic lg:text-[4rem] text-[2rem] lg:left-[105px] left-4 lg:bottom-32 bottom-48">
+                  {slideImage.caption2}
+                </p>
+                <p className="lg:left-[105px] font-nunitoSans absolute font-light text-fondoBlanco md:text-[2rem] text-[1.5rem] left-4 lg:bottom-24 bottom-36">
+                  {slideImage.caption}
+                </p>
+              </div>
+            </Link>
           </div>
         ))}
       </Slide>
