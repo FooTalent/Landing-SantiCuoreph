aa1129cd746772ff7bc9e8efbc7c08e492387b0d
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-04-11 20:54:51 -0300

    Add responsive font size to service banner

--- src/app/servicios/components/ServiceBanner.tsx
+++ src/app/servicios/components/ServiceBanner.tsx
@@ -33,7 +33,7 @@ const ServiceBanner = () => {
       >
         {images.map((image, idx) => {
           return (
-            <div key={idx} className="each-slide">
+            <div key={idx} className="each-slide mb-[-1px]">
               <div className="relative">
                 <Image
                   src={image.url}
@@ -44,15 +44,15 @@ const ServiceBanner = () => {
                   style={{objectPosition: "50% 20%"}}
                   alt="Imagen 01"
                 />
-                <div className="absolute flex flex-col md:flex-row inset-0 px-4 xl:max-w-screen-xl xl:px-0 mx-auto items-center gap-10 sm:gap-16 md:gap-0 pt-16 sm:pt-24 md:pt-0 lg:flex-nowrap xl:gap-20">
+                <div className="absolute flex flex-col md:flex-row inset-0 px-4 xl:max-w-screen-xl xl:px-0 mx-auto items-center gap-10 sm:gap-16 md:gap-2 pt-16 sm:pt-24 md:pt-0 lg:flex-nowrap xl:gap-20">
                   <div className="">
-                    <h3 className="text-xl text-center md:text-left sm:text-3xl xl:text-[43px] text-fondoBlanco font-nunitoSans font-bold" style={{lineHeight: "50px"}}>
+                    <h3 className="text-lg text-center md:text-left sm:text-3xl lg:text-4xl xl:text-[43px] text-fondoBlanco font-nunitoSans font-bold leading-[25px]">
                       {image.caption}
                     </h3>
                   </div>
-                  <div className="w-full md:w-1/3">
+                  <div className="w-full md:w-1/3 px-4 md:px-0">
                     <Link href="/contacto">
-                      <h3 className="text-lg sm:text-2xl  bg-principal hover:bg-principalHover hover:font-bold text-fondoNegro text-center rounded-full merriwather font-semibold py-[0.5em] md:px-5 xl:px-[80px]">Contactame</h3>
+                      <h3 className="text-md sm:text-2xl  bg-principal hover:bg-principalHover hover:font-bold text-fondoNegro text-center rounded-full merriwather font-semibold py-[0.5em] md:px-5 xl:px-[80px]">Contactame</h3>
                     </Link>
 
                   </div>
