5ff6829dbf7deb7979fd56bc4564d275f01ee39f
JoelRondinelPacheco <joel.rondinel.pacheco@gmail.com>
2024-04-10 22:22:16 -0300

    Add full responsive service section grid

--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -10,27 +10,25 @@ import img06 from "../../../public/images/services/photography/img06.jpg";
 import img07 from "../../../public/images/services/photography/img07.jpg";
 import flecha from "../Icon/Group.png";
 import Link from "next/link";
-//md:max-h-[600px] lg:max-h-[800px]
-//tamanos foto 2 md:max-h-[300px] lg:max-h-[400px] xl:max-h-[464px]
-//TODO AGREGAR OPACITY 0
+
 const ServicesPage = () => {
   return (
     <div className="mx-4">
       <section className="xl:max-w-screen-xl mb-16  mx-auto md:mb-36 text-fondoBlanco  md:px-0 mt-6 md:mt-10">
-        <div className="md:grid md:grid-cols-12 md:grid-rows-4 md:gap-3 mb-3 items-stretch overflow-hidden lg:h-[941px] space-y-4 md:space-y-4">
-          <div className="md:col-span-5 md:col-start-8 h-[654px] lg:h-[941px]">
+        <div className="md:grid md:grid-cols-12 md:grid-rows-4 md:gap-3 mb-3 items-stretch overflow-hidden md:h-[800px] lg:h-[941px] space-y-4 md:space-y-0">
+          <div className="md:col-span-5 md:col-start-8 h-[654px] md:h-[800px] lg:h-[941px] rounded-2xl">
             <Link
               href="/fotografia/lorena"
               className="block md:col-span-2 relative h-full w-full group "
               id="0"
             >
               <div
-                className="texto_image w-full absolute bottom-1/2 top-2/4  z-30 h-28 md:h-40 flex flex-row text-center px-4 md:px-10
+                className="texto_image w-full absolute bottom-1/2 top-2/4  z-30 h-28 md:h-40 flex flex-row text-center px-4 sm:px-10
             justify-between items-center cursor-pointer transition-all duration-200 bg-black/50"
               >
-                <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
-                  <h3 className="text-2xl md:text-3xl md:mb-4 italic">Retrato Artístico</h3>
-                  <h2 className="text-md md:text-2xl font-thin">Moda subte</h2>
+                <div className="md:group-hover:pr-10  w-full  transition-all duration-300">
+                  <h3 className="text-2xl sm:text-3xl md:mb-4 italic">Retrato Artístico</h3>
+                  <h2 className="text-md sm:text-2xl font-thin">Moda subte</h2>
                 </div>
                 <Image
                   src={flecha}
@@ -41,7 +39,7 @@ const ServicesPage = () => {
               <Image
                 src={img01}
                 alt="asd"
-                className="object-cover  h-full rounded-2xl  group-hover:brightness-50"
+                className="object-cover md:h-[800px] lg:h-[941px] rounded-2xl  group-hover:brightness-50"
               />
             </Link>
           </div>
@@ -52,9 +50,9 @@ const ServicesPage = () => {
               className="relative md:pt-0 group"
               id="1"
             >
-              <div className="texto_image bottom-0 rounded-b-2xl absolute z-30 w-full h-1/3 MD:h-40 flex flex-row text-center justify-between px-4 md:px-10 items-center cursor-pointer transition-all duration-200 bg-black/50">
-                <div className="md:group-hover:pr-20 w-full  transition-all duration-300">
-                <h3 className="text-2xl md:text-3xl md:mb-4 italic">
+              <div className="texto_image bottom-0 rounded-b-2xl absolute z-30 w-full h-1/3 MD:h-40 flex flex-row text-center justify-between px-4 sm:px-10 items-center cursor-pointer transition-all duration-200 bg-black/50">
+                <div className="md:group-hover:pr-5 w-full  transition-all duration-300">
+                <h3 className="text-2xl md:text-3xl md:pb-2 lg:mb-4 italic">
                     Fotografía comercial
                   </h3>
                   <h2 className="text-md md:text-2xl font-thin">AURA Gin</h2>
@@ -68,7 +66,7 @@ const ServicesPage = () => {
               <Image
                 src={img02}
                 alt="asd"
-                className="object-cover rounded-b-2xl h-full rounded-2xl group-hover:brightness-50 h-[235px] sm:h-[300px]"
+                className="object-cover rounded-b-2xl rounded-2xl group-hover:brightness-50 h-[235px] sm:h-[300px] md:h-[395px] lg:h-[465px]"
               />
             </Link>
           </div>
@@ -79,8 +77,8 @@ const ServicesPage = () => {
               className="relative group"
               id="2"
             >
-              <div className="texto_image bottom-0 rounded-b-2xl absolute z-30 px-10 w-full h-1/3 md:h-40 flex  flex-row text-center justify-between px-4 md:px-10 items-center cursor-pointer transition-all duration-200 bg-black/50">
-                <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
+              <div className="texto_image bottom-0 rounded-b-2xl absolute z-30 px-10 w-full h-1/3 md:h-40 flex  flex-row text-center justify-between px-4 sm:px-10 items-center cursor-pointer transition-all duration-200 bg-black/50">
+                <div className="md:group-hover:pr-10  w-full  transition-all duration-300">
                   <h3 className="text-2xl md:text-3xl md:mb-4 italic">
                     Fotografía artística
                   </h3>
@@ -95,19 +93,19 @@ const ServicesPage = () => {
               <Image
                 src={img03}
                 alt="asd"
-                className="object-cover rounded-b-2xl h-full rounded-2xl group-hover:brightness-50 h-[235px] sm:h-[300px]"
+                className="object-cover rounded-b-2xl h-full rounded-2xl group-hover:brightness-50 h-[235px] sm:h-[300px] md:h-[400px] lg:h-[465px]"
               />
             </Link>
           </div>
         </div>
-        <div>
+        <div >
           <Link
             href="/fotografia/jettatore"
-            className="mb-3 flex h-[138px] sm:h-[250px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] relative group"
+            className="mb-3 block h-[138px] sm:h-[250px] md:h-[300px] lg:h-[434px] relative group "
             id="3"
           >
-            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/2 md:h-40 flex flex-row text-center px-4 md:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
-              <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
+            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/2 md:h-1/3 flex flex-row text-center px-4 sm:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
+              <div className="md:group-hover:pr-10  w-full  transition-all duration-300">
                 <h3 className="text-2xl md:text-3xl md:mb-4 italic">
                   Evento artístico
                 </h3>
@@ -122,17 +120,17 @@ const ServicesPage = () => {
             <Image
               src={img04}
               alt="asd"
-              className="object-cover rounded-b-2xl h-[138px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] object-top rounded-2xl  group-hover:brightness-50"
+              className="object-cover rounded-b-2xl h-full h-[138px] sm:h-[250px] md:h-[300px] lg:h-[434px] object-top rounded-2xl  group-hover:brightness-50"
             />
           </Link>
         </div>
-        <div className="md:grid md:grid-cols-2 md:gap-3 mb-3 mt-3 md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] ">
+        <div className="md:grid md:grid-cols-2 md:gap-3 mb-3 mt-3">
           <Link
             href="/fotografia/one-bar"
-            className="h-[291px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] relative pb-3 md:pb-0 group flex mb-3 md:mb-0"
+            className="h-[291px] sm:h-[320px] lg:h-[458px] relative pb-3 md:pb-0 group flex mb-3 md:mb-0"
           >
-            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/3 md:h-40 flex  flex-row text-center px-4 md:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
-              <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
+            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/3 md:h-40 flex  flex-row text-center px-4 sm:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
+              <div className="md:group-hover:pr-5  w-full  transition-all duration-300">
                 <h3 className="text-2xl md:text-3xl md:mb-4 italic">
                   Evento social
                 </h3>
@@ -147,15 +145,16 @@ const ServicesPage = () => {
             <Image
               src={img05}
               alt="asds"
-              className="object-cover rounded-b-2xl h-[291px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] object-top rounded-2xl  group-hover:brightness-50"
+              className="object-cover rounded-b-2xl h-[291px] sm:h-[320px] lg:h-[458px] md:object-top rounded-2xl  group-hover:brightness-50"
+              style={{objectPosition: "50% 10%"}}
             />
           </Link>
           <Link
             href="/fotografia/mas-que-uno"
-            className="col-span-1 h-full relative group flex"
+            className="col-span-1 h-[267px] sm:h-[300px] md:h-[320px] lg:h-[458px] relative group flex"
           >
-            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/3 md:h-40 flex flex-row text-center px-4 md:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
-              <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
+            <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-1/3 md:h-40 flex flex-row text-center px-4 sm:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
+              <div className="md:group-hover:pr-3  w-full  transition-all duration-300">
                 <h3 className="text-2xl md:text-3xl md:mb-4 italic">
                   Evento artístico
                 </h3>
@@ -170,7 +169,7 @@ const ServicesPage = () => {
             <Image
               src={img06}
               alt="asds"
-              className="object-cover h-full rounded-b-2xl h-[267px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] rounded-2xl  group-hover:brightness-50"
+              className="object-cover rounded-b-2xl object-top h-[267px] sm:h-[300px] md:h-[320px] lg:h-[458px] rounded-2xl  group-hover:brightness-50"
             />
           </Link>
         </div>
@@ -178,8 +177,8 @@ const ServicesPage = () => {
           href="/fotografia/mas-que-uno"
           className="mb-3 flex relative group"
         >
-          <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-[69px] md:h-40 flex flex-row gap-2 text-center px-4 md:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
-            <div className="md:group-hover:pr-20  w-full  transition-all duration-300">
+          <div className="texto_image absolute bottom-0 rounded-b-2xl z-30 w-full h-[69px] sm:h-[80px] md:h-40 flex flex-row gap-2 text-center px-4 sm:px-10 justify-between items-center cursor-pointer transition-all duration-200 bg-black/50">
+            <div className="md:group-hover:pr-10  w-full  transition-all duration-300">
               <h3 className="text-2xl md:text-3xl md:mb-4 italic">
                 Evento artístico
               </h3>
@@ -194,7 +193,7 @@ const ServicesPage = () => {
           <Image
             src={img07}
             alt="asds"
-            className="object-cover rounded-b-2xl h-[138px] md:max-h-[300px] lg:max-h-[400px] xl:max-h-[456px] object-top rounded-2xl group-hover:brightness-50 transition-all duration-300"
+            className="object-cover rounded-b-2xl h-[138px] sm:h-[200px] md:h-[300px] lg:h-[434px] object-top rounded-2xl group-hover:brightness-50 transition-all duration-300"
           />
         </Link>
       </section>
